0c93564af5121bcb400cbc33d49c99ef8af27fd2
Jared <Jtruscott78@gmail.com>
2017-07-16 09:50:05 -0400

    completed checkpoint 9

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,12 +1,25 @@
-  var points = document.getElementsByClassName('point');
+var pointsArray = document.getElementsByClassName('point');
 
-function revealPoints(numberOfIndexes) {
-  for (var i = 0; i < numberOfIndexes; i++) {
-    points[i].style.opacity = 1;
-    points[i].style.transform = "scaleX(1) translateY(0)";
-    points[i].style.msTransform = "scaleX(1) translateY(0)";
-    points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
-  };
+var animatePoints = function(points) {
+  var revealPoint = function(index) {
+    points[index].style.opacity = 1;
+    points[index].style.transform = "scale(1) translateY(0)";
+    points[index].style.msTransform = "scale(1) translateY(0)";
+    points[index].style.WebkitTransform = "scale(1) translateY(0)";
+  }
+
+  for (var i = 0; i < points.length; i++) {
+    revealPoint(i);
+  }
+};
+
+window.onload = function() {
+  var sellingPoints = document.getElementsByClassName('selling-points')[0];
+  var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+
+    window.addEventListener("scroll", function(event) {
+      if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+             animatePoints(pointsArray);
+         }
+    });
 }
-
-revealPoints(points.length);
